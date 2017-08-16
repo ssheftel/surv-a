447a8036be3b59a7b99a1b1c2cbf2358c56b56df
Sam Sheftel <mathias@mailinator.com>
2017-08-15 22:02:53 -0500

    procfile

---
+++ settings.py
@@ -0,0 +1,17 @@
+
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+import os
+
+MONGO_HOST = os.environ.get('MONGO_HOST')
+MONGO_PORT = int(os.environ.get('MONGO_PORT'))
+MONGO_USERNAME = os.environ.get('MONGO_USERNAME')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
+
+
+DOMAIN = {}
+
